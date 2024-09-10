e9c32391d1521a73480179526efb275eabc1d111
Moayed Alhagy <moayedlaravel@gmail.com>
2024-09-10 10:55:37 +0300

    add animations

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,24 +4,17 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="hero pt-20 text-center w-full   flex flex-col text-white justify-center items-center gap-y-5">
-      <h1 className="text-4xl md:text-5xl lg:text-7xl font-semibold leading-9">
+      <h1 className="text-4xl md:text-5xl lg:text-7xl font-semibold leading-9 ">
         {" "}
         الجامعة الإسلامية
         <span className="my-7  block"> فـي سـوريـا</span>
       </h1>
-      <div className="mt-6 hidden">
-        <Image
-          src={"/images/in_syria_text.png"}
-          width={"230"}
-          height={"230"}
-          alt="logo"
-        />
+
+      <div className="mt-6 text-lg md:text-xl ">
+        <p className="typewriter">صرح علمي يجمع تخصصات فريـدة</p>
+        <p className="  typewriter  pt-1">ضمن بيئة معرفيـة متطورة</p>
       </div>
-      <p className="mt-6 text-lg md:text-xl ">
-        صرح علمي يجمع تخصصات فريـدة
-        <span className="my-3  block">ضمن بيئة معرفيـة متطورة</span>
-      </p>
-      <div className="my-8">
+      <div className="my-8 animate-slideInFromBottom">
         <Image
           src={"/images/logo.svg"}
           width={"150"}
