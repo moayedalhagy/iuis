366100b26f34f7c832da3443e77ac1cd73bf7174
Moayed Alhagy <moayedlaravel@gmail.com>
2024-09-11 14:27:19 +0300

    add som files for web hosting and add seo attributes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,52 @@ import type { Metadata } from "next";
 import { Alexandria } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
+import type { Viewport } from "next";
+import "../styles/globals.css";
 import "react-toastify/ReactToastify.min.css";
+
 const alexandria = Alexandria({
   weight: ["400", "700"],
   subsets: ["arabic", "latin"],
 });
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
+
 export const metadata: Metadata = {
   title: "IUIS Unversity",
-  description: "الجامعة الإسلامية في سوريا",
+  description:
+    "الجامعة الإسلامية في سوريا تقدم تعليمًا عالي الجودة في مختلف المجالات الأكاديمية والدينية.",
+  keywords:
+    "IUIS, Islamic University, الجامعة الإسلامية, Syria, education , في سوريا",
+  authors: { name: "IUIS Unversity" },
+  robots: "index, follow",
+  metadataBase: new URL("https://iuis.university"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      en: "/en",
+      ar: "/ar",
+    },
+  },
+  openGraph: {
+    title: "IUIS University",
+    description:
+      "الجامعة الإسلامية في سوريا تقدم تعليمًا عالي الجودة في مختلف المجالات الأكاديمية والدينية.",
+    url: "https://iuis.university",
+    type: "website",
+    images: ["/images/seo_logo.png"],
+  },
+  icons: {
+    icon: "favicon.ico",
+  },
 };
 
 export default function RootLayout({
