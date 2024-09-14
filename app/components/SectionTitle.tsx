52c798269a5fb71d6ed7c9c5acdd11409fb85b02
Moayed Alhagy <moayedlaravel@gmail.com>
2024-09-14 12:48:37 +0300

    section title remove line above text

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -4,7 +4,7 @@ type SectionTitleProps = {
 export default function SectionTitle({ text }: SectionTitleProps) {
   return (
     <div className="flex flex-col gap-y-1">
-      <div className="h-1 w-[25px] self-start bg-[#03A679] md:h-1 md:w-[35px]"></div>
+
       <h1 className="gradient-text py-3.5 text-2xl font-bold md:text-4xl">
         {text}
       </h1>
