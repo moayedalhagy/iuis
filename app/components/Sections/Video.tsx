60e98582ec803bec7244651e904e1035388ad037
Moayed Alhagy <moayedlaravel@gmail.com>
2025-04-23 05:26:36 +0300

    fix

--- app/components/Sections/Video.tsx
+++ app/components/Sections/Video.tsx
@@ -14,8 +14,8 @@ export default async function Video() {
             className="rounded-xl border-0 outline-0"
             width="853"
             height="480"
-            // src="https://www.youtube.com/embed/8UYy6yRttYw"
-            src={videoData[0].videoUrl}
+            src="https://www.youtube.com/embed/8UYy6yRttYw"
+            // src={videoData[0].videoUrl}
             title="المقدمة التعريفية للجامعة الإسلامية"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
